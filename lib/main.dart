87fc31d63dde7e64c7e1b5b46851f6ae95e00960
Shivansh722 <113300509+Shivansh722@users.noreply.github.com>
2023-10-22 16:25:48 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:fakestore/model/colors.dart';
+import 'package:fakestore/model/l10n/l10n.dart';
+import 'package:fakestore/model/routes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:get/get.dart';
